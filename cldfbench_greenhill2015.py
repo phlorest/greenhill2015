af58898c168398f061e2bc86a0e1957e90d9b51f
SimonGreenhill <simon@simon.net.nz>
2022-04-22 22:26:03 +1200

    update

--- cldfbench_greenhill2015.py
+++ cldfbench_greenhill2015.py
@@ -9,22 +9,15 @@ class Dataset(phlorest.Dataset):
 
     def cmd_makecldf(self, args):
         self.init(args)
-        args.writer.add_summary(
-            self.raw_dir.read_tree(
-                'mcelhanon-1967-covarion.mcct.trees', detranslate=True),
-            self.metadata,
-            args.log)
         
-        posterior = self.sample(
-            self.remove_burnin(
-                self.raw_dir.read('mcelhanon-1967-covarion.trees.gz'),
-                10001),
-            detranslate=True,
-            as_nexus=True)
-        args.writer.add_posterior(
-            posterior.trees.trees,
-            self.metadata,
-            args.log)
+        summary = self.raw_dir.read_tree(
+            'mcelhanon-1967-covarion.mcct.trees', detranslate=True)
+        args.writer.add_summary(summary, self.metadata, args.log)
+
+        posterior = self.raw_dir.read_trees(
+           'mcelhanon-1967-covarion.trees.gz',
+           burnin=10001, sample=1000, detranslate=True)
+        args.writer.add_posterior(posterior, self.metadata, args.log)
 
         args.writer.add_data(
             self.raw_dir.read_nexus('mcelhanon-1967.nex'),
